d159d6f97790ec54cfc926c454c3559db98e6a4d
Cally Sibben <themodempress@gmail.com>
2014-08-28 02:22:30 -0500

    Basic menu framework added and partially implemented

--- PCP-RPG/pcp-rpg.cpp
+++ PCP-RPG/pcp-rpg.cpp
@@ -9,6 +9,27 @@ namespace RPG
 {
 	::string re;
 	bool isQuit = false;
+	int menu = 1;
+	namespace menus
+	{
+		void menuMain()
+		{
+			::cout << "PCP-RPG" << ::endl;
+			::cout << ::endl;
+			::cout << "[1]    New Game" << ::endl;
+			::cout << "[2]    Load Game" << ::endl;
+			::cout << "[3]    Options" << ::endl;
+			::cout << "[4]    Credits" << ::endl;
+			::cout << "[5]    Quit" << ::endl;
+		}
+		void checkMenu()
+		{
+			if (menu = 1)
+			{
+				menuMain();
+			}
+		}
+	}
 };
 
 using namespace RPG;
@@ -17,12 +38,7 @@ int main()
 {
 	while (!::isQuit)
 	{
+		RPG::menus::checkMenu();
 		std::getline(std::cin, ::re);
-		if (::re == "quit")
-		{
-			::isQuit = true;
-			break;
-		}
-		std::cout << ::re << std::endl;
 	}
 }
