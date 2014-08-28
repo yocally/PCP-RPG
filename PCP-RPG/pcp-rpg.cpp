69aa5d5df20454dc44a0c0096941de104a06c819
Cally Sibben <themodempress@gmail.com>
2014-08-28 00:38:36 -0500

    Got basic menu control prototyped

--- PCP-RPG/pcp-rpg.cpp
+++ PCP-RPG/pcp-rpg.cpp
@@ -1,8 +1,28 @@
 
 #include <iostream>
 #include <stdio.h>
+#include <string>
+
+using namespace std;
+
+namespace RPG
+{
+	::string re;
+	bool isQuit = false;
+};
+
+using namespace RPG;
 
 int main()
 {
-
+	while (!::isQuit)
+	{
+		std::getline(std::cin, ::re);
+		if (::re == "quit")
+		{
+			::isQuit = true;
+			break;
+		}
+		std::cout << ::re << std::endl;
+	}
 }
